dcf9e01629cd8a7a3c1360749577c75de72351bc
Xeshan07 <86343514+Xeshan07@users.noreply.github.com>
2021-09-28 11:10:30 +0500

    add connection file

---
+++ connection.php
@@ -0,0 +1,31 @@
+<?php
+
+
+
+//database_connection.php
+
+
+
+$con =mysqli_connect("localhost","root","","icecream");
+
+
+
+// $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// echo "Connected successfully";
+
+// } 
+
+
+
+// catch(PDOException $e) {
+
+
+
+// echo "Connection failed: " . $e->getMessage();
+
+// }
+
+
+
+?>
